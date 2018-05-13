982a3cc309207799c794374ca320204ccd14f858
Carlos Freire Acebedo <desbarrao@gmail.com>
2018-05-13 22:24:49 +0200

    Refactorizado código C# usando LinQ

---
+++ csharp/GildedRose.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+
+        const string AGEDBRIE = "Aged Brie";
+        const string BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+        const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+        public GildedRose(IList<Item> Items)
+        {
+            CheckRepeatItems(Items);
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            Items.Where(x => x.Name != AGEDBRIE && x.Name != BACKSTAGE && x.Name != SULFURAS && x.Quality > 0).ToList().ForEach(x => x.Quality -= 1 );
+            Items.Where(x => (x.Name == AGEDBRIE || x.Name == BACKSTAGE) && x.Quality < 50).ToList().ForEach(x => x.Quality += 1);
+            Items.Where(x => x.Name == BACKSTAGE && x.Quality < 50 && x.SellIn < 11).ToList().ForEach(x => x.Quality += 1);
+            Items.Where(x => x.Name == BACKSTAGE && x.Quality < 50 && x.SellIn < 6).ToList().ForEach(x => x.Quality += 1);
+            Items.Where(x => x.Name != SULFURAS).ToList().ForEach(x => x.SellIn -= 1);
+            Items.Where(x => x.SellIn < 0 && x.Name == AGEDBRIE && x.Quality < 50).ToList().ForEach(x => x.Quality += 1);
+            Items.Where(x => x.SellIn < 0 && x.Name != AGEDBRIE && x.Name != BACKSTAGE && x.Name != SULFURAS && x.Quality > 0).ToList().ForEach(x => x.Quality -= 1);
+            Items.Where(x => x.SellIn < 0 && x.Name == BACKSTAGE).ToList().ForEach(x => x.Quality = 0);
+        }
+
+        private void CheckRepeatItems(IList<Item> Items)
+        {
+            if (Items.Count != Items.Select(i => i.Name.ToUpper()).Distinct().Count())
+            {
+                throw new Exception("Repeat items");
+            }
+        }
+    }
+}
---
+++ csharp/GildedRoseTest.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [Test]
+        public void QualityDown()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(3, Items[0].Quality);
+        }
+
+        [Test]
+        public void AgedBrie()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(6, Items[0].Quality);
+            Assert.AreEqual(0, Items[0].SellIn);
+            app.UpdateQuality();
+            Assert.AreEqual(8, Items[0].Quality);
+            Assert.AreEqual(-1, Items[0].SellIn);
+        }
+
+        [Test]
+        public void Max50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+
+        [Test]
+        public void Sulfuras()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 5, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(5, Items[0].Quality);
+            Assert.AreEqual(5, Items[0].SellIn);
+        }
+
+        [Test]
+        public void Backstage()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(6, Items[0].Quality);
+            app.UpdateQuality();
+            Assert.AreEqual(8, Items[0].Quality);
+            Items[0].SellIn = 5;
+            Items[0].Quality = 5;
+            app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(8, Items[0].Quality);
+            Items[0].SellIn = 0;
+            Items[0].Quality = 5;
+            app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+    }
+}
